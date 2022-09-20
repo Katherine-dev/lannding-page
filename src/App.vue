bebd1ca1e9bddc0702f8fe0b108ae1ef2fc36c64
Katherinde-dev <evorotnikowa@yandex.ru>
2022-09-20 20:39:46 +0300

    add hero view

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <navbar/>
+    <header-page/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderPage from './views/Header/index.vue';
+import Navbar from './components/Navbar.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    HeaderPage,
+    Navbar,
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style>
+<style lang='scss'>
+@import './scss/style.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @include regular
+}
+
+.container {
+  max-width: 1300px;
+  margin: auto;
+}
+
+@media (max-width: 1299px) {
+  .container {
+    max-width: 1100px;
+  }
+}
+@media (max-width: 1199px) {
+  .container {
+    max-width: 960px;
+  }
+}
+@media (max-width: 991px) {
+  .container {
+    max-width: 750px;
+  }
+}
+@media (max-width: 768px) {
+  .container {
+    max-width: 560px;
+  }
 }
 </style>
---
+++ src/views/Header/index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container hero-wrap">
+      <img class="bg-dots" src="../../assets/icons/dots.svg" alt="">
+
+      <div class="text-container">
+        <span class="bold-text"> Virtual healthcare<br/>for you</span>
+        <span class='regular-text'>Trafalgar provides progressive, and affordable
+        <br/>healthcare, accessible on mobile and online<br/>for everyone</span>
+        <button class='button-primary'>Consult today</button>
+      </div>
+
+      <div class="image-container">
+        <img src="../../assets/images/doctors.svg" alt="">
+      </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Header extends Vue {}
+</script>
+
+<style lang='scss'>
+  @import '../../scss/style.scss';
+
+  .hero-wrap {
+    display: flex;
+    align-items: center;
+    gap: 100px;
+
+    position: relative;
+  }
+  .text-container {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+  }
+  .bold-text {
+    @include bold;
+    font-size: 48px;
+  }
+
+  .regular-text {
+    font-size: 21px;
+    margin-top: 20px;
+  }
+
+  .image-container {
+    width: 50%;
+    img {
+      width: 100%;
+    }
+  }
+
+  .button-primary {
+    width: 30%;
+    height: 60px;
+    margin-top: 40px;
+
+    background-color: $primary-color;
+    color: #fff;
+
+    border: none;
+    border-radius: 55px;
+  }
+
+  .bg-dots {
+    position: absolute;
+    top: 0;
+    left: -150px;
+  }
+</style>
